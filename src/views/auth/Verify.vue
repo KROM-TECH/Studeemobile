c1fd859cf34498777c0d11e0e5721d48b9964f03
kromate <akpananthony33@gmail.com>
2021-01-23 11:35:12 +0100

    verify text update

--- src/views/auth/Verify.vue
+++ src/views/auth/Verify.vue
@@ -6,8 +6,8 @@
       <h2 style="margin:0" class="center">
         verify your Email to Proceed
       </h2>
-      <button v-if="btn" class="btn Obtn " @click="verify">Verify</button>
-      <p v-else class="center pur-text">{{ msg }}</p>
+      <button v-if="!btn" class="btn Obtn " @click="verify">Verify</button>
+      <p v-else class="center pur-text" v-html="msg"></p>
     </div>
   </div>
 </template>
@@ -37,13 +37,13 @@ export default {
           console.log(firebase.auth().currentUser);
           this.loader = false;
           this.btn = false;
-          this.msg = `A Verification Link has been sent to Your Email `;
+          this.msg = `A Verification Link has been sent to Your Email <br/> After Verification sign out and sign in to access your account`;
         })
         .catch((error) => {
           console.log(error.message);
           this.loader = false;
           this.btn = true;
-          this.msg = `Something went wrong, check your network and then reload the page`;
+          this.msg = `Something went wrong, check your network and then reload the page `;
         });
     },
   },
